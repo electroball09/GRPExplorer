e6de603d4c33edd7d1b3d3dd5cfd28f584dc748e
electro <edgrbrmn@gmail.com>
2019-07-11 22:43:15 -0600

    reorganized some code

--- UnityIntegration/TextureLoaderTest.cs
+++ UnityIntegration/TextureLoaderTest.cs
@@ -105,11 +105,11 @@ namespace UnityIntegration
         {
             if (Transparent)
             {
-                testPlane.GetComponent<Renderer>().material.ChangeRenderMode(StandardShaderUtils.BlendMode.Transparent);
+                testPlane.GetComponent<Renderer>().material.ChangeRenderMode(BlendMode.Transparent);
             }
             else
             {
-                testPlane.GetComponent<Renderer>().material.ChangeRenderMode(StandardShaderUtils.BlendMode.Opaque);
+                testPlane.GetComponent<Renderer>().material.ChangeRenderMode(BlendMode.Opaque);
             }
 
             loadedPayload?.Unload();
@@ -131,60 +131,5 @@ namespace UnityIntegration
 
             testPlane.GetComponent<Renderer>().material.mainTexture = texture;
         }
-    }
-}
-
-public static class StandardShaderUtils
-{
-    public enum BlendMode
-    {
-        Opaque,
-        Cutout,
-        Fade,
-        Transparent
-    }
-
-    public static void ChangeRenderMode(this Material standardShaderMaterial, BlendMode blendMode)
-    {
-        switch (blendMode)
-        {
-            case BlendMode.Opaque:
-                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-                standardShaderMaterial.SetInt("_ZWrite", 1);
-                standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
-                standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
-                standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-                standardShaderMaterial.renderQueue = -1;
-                break;
-            case BlendMode.Cutout:
-                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-                standardShaderMaterial.SetInt("_ZWrite", 1);
-                standardShaderMaterial.EnableKeyword("_ALPHATEST_ON");
-                standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
-                standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-                standardShaderMaterial.renderQueue = 2450;
-                break;
-            case BlendMode.Fade:
-                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-                standardShaderMaterial.SetInt("_ZWrite", 0);
-                standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
-                standardShaderMaterial.EnableKeyword("_ALPHABLEND_ON");
-                standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-                standardShaderMaterial.renderQueue = 3000;
-                break;
-            case BlendMode.Transparent:
-                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-                standardShaderMaterial.SetInt("_ZWrite", 0);
-                standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
-                standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
-                standardShaderMaterial.EnableKeyword("_ALPHAPREMULTIPLY_ON");
-                standardShaderMaterial.renderQueue = 3000;
-                break;
-        }
-
     }
 }
