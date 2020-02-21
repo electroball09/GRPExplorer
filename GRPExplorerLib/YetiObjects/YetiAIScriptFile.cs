19b925864af18865bd9c3a7290c2d8fcf72a91be
electro <edgrbrmn@gmail.com>
2020-02-20 19:02:12 -0700

    add ai script file archetype

---
+++ GRPExplorerLib/YetiObjects/YetiAIScriptFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GRPExplorerLib.Logging;
+using GRPExplorerLib.BigFile;
+using System.IO;
+
+namespace GRPExplorerLib.YetiObjects
+{
+    public class YetiAIScriptFile : YetiObjectArchetype
+    {
+        public override YetiObjectType Identifier => YetiObjectType.zc_;
+
+        public string Script { get; private set; }
+
+        public override void Load(byte[] buffer, int size, YetiObject[] objectReferences)
+        {
+            Script = Encoding.ASCII.GetString(buffer, 0, size);
+        }
+
+        public override void Log(ILogProxy log)
+        {
+            log.Info("YETI AI SCRIPT {0}", Object.Name);
+            log.Info("\n" + Script);
+            log.Info("");
+        }
+    }
+}
